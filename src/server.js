8c763af78315b47142b5f2763812d8382c289684
Kevin <kevin.zookski@gmail.com>
2025-06-19 20:14:15 -0500

    adding cloudflare IP to allowed origins in CORS

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ const corsOptions = {
         'http://127.0.0.1:3000',
         'http://127.0.0.1:3001',
         'http://192.168.1.225:3000',
+        'http://172.17.0.1:3000'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
