c836947ba46cb3d9b28f37472474a562526ce42b
Kevin <kevin.zookski@gmail.com>
2025-06-20 20:02:02 -0500

    fixing lint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,  // switching to Node since that's what we currently use
+        ...globals.es2021
+      },
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      // Rules go here
+    }
+  }
+];
